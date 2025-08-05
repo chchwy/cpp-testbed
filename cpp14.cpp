e3671a39da32ba42a87b58707ff75faa05198032
Matt Chang <chchwy@gmail.com>
2025-08-05 10:43:44 +0800

    C++14 bit literal

---
+++ cpp14.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "doctest.h"
+
+TEST_CASE("Test bit literals") {
+    CHECK(0b0 == 0);
+    CHECK(0b1 == 1);
+    CHECK(0b10 == 2);
+    CHECK(0b11 == 3);
+    CHECK(0b100 == 4);
+    CHECK(0b1010 == 10);
+    CHECK(0b1111 == 15);
+    CHECK(0b10000000 == 128);
+    CHECK(0b11111111 == 255);
+    CHECK(0b101010101010 == 0xAAA);
+    CHECK(0b1111111111111111 == 0xFFFF);
+    CHECK(0b0000100000000000 == (1 << 11));
+}
