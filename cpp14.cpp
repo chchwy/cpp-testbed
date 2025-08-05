6250a2cbcef3f9193f5800672185d94f476c5fa9
Matt Chang <chchwy@gmail.com>
2025-08-05 12:15:12 +0800

    Update C++14 binary literal

--- cpp14.cpp
+++ cpp14.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <bitset>
 #include "doctest.h"
 
-TEST_CASE("Test bit literals") {
+TEST_CASE("C++14 Binary Literals") {
     CHECK(0b0 == 0);
     CHECK(0b1 == 1);
     CHECK(0b10 == 2);
@@ -14,4 +15,7 @@ TEST_CASE("Test bit literals") {
     CHECK(0b101010101010 == 0xAAA);
     CHECK(0b1111111111111111 == 0xFFFF);
     CHECK(0b0000100000000000 == (1 << 11));
+    CHECK(0b011101110111 == 0x777);
+    std::bitset<8> bits(0b10000000);
+    CHECK(bits.to_ulong() == 128);
 }
