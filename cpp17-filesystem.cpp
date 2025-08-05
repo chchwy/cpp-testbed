984fafefa3661bc01d136c14fc3652f51020d493
Matt Chang <chchwy@gmail.com>
2025-08-05 12:36:14 +0800

    Test filesystem::path CJK paths

--- cpp17-filesystem.cpp
+++ cpp17-filesystem.cpp
@@ -2,7 +2,7 @@
 #include <filesystem>
 
 TEST_CASE("Testing filesystem") {
-    std::string path = "C:/path/to/directory";
+    std::string path = "C:/path/許功蓋/directory";
     std::filesystem::path dirPath(path);
 
     CHECK(dirPath.string() == path);
